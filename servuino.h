809f208f730d7712a8114e27cacacf4459655fb5
benny.saxen <benny.saxen@80dbc977-2909-00a8-689b-46a85cf51ee1>
2011-12-30 21:50:31 +0000

    String part 1

--- servuino.h
+++ servuino.h
@@ -1,101 +1,122 @@
 void passTime();
 void digitalWrite(int pin,int value);
 int  digitalRead(int pin);
+//=====================================
+// String
+//=====================================
+
 #include <iostream>
 using std::cerr;
 using std::cout;
 using std::endl;
+using std::ostream;
+using std::istream;
 
 #include <iomanip>
 using std::setw;
 
-#include <cstring> // strcpy and strcat prototypes
+#include <cstring> 
 using std::strcmp;
 using std::strcpy;
 using std::strcat;
 
-#include <cstdlib> // exit prototype
+#include <string> 
+using std::string;
+
+#include <cstdlib>
 using std::exit;
 
 #include <sstream>
-
+using std::stringstream;
 
 class String
 {
    friend ostream &operator<<( ostream &, const String & );
    friend istream &operator>>( istream &, String & );
+
 public:
    String( const char * = "" ); // conversion/default constructor
    String( const String & ); // copy constructor
    ~String(); // destructor
 
    const String &operator=( const String & ); // assignment operator
-   const String &operator=( string right );
-   const String &operator=( const char *right );
    const String &operator+=( const String & ); // concatenation operator
-   String operator+( const int number ); // concatenation operator
-   String operator+( const char one );
-   String operator+( unsigned long number );
-   String operator+( String right );
-   //String string::operator+( String right ) const;
 
    bool operator!() const; // is String empty?
    bool operator==( const String & ) const; // test s1 == s2
    bool operator<( const String & ) const; // test s1 < s2
 
-   // test s1 != s2
    bool operator!=( const String &right ) const
    { 
       return !( *this == right ); 
-   } // end function operator!=
+   } 
 
-   // test s1 > s2
    bool operator>( const String &right ) const
    { 
       return right < *this; 
-   } // end function operator>
+   } 
  
-   // test s1 <= s2
    bool operator<=( const String &right ) const
    { 
       return !( right < *this ); 
-   } // end function operator <=
+   }
 
-   // test s1 >= s2
    bool operator>=( const String &right ) const
    { 
       return !( *this < right ); 
-   } // end function operator>=
+   } 
 
    char &operator[]( int ); // subscript operator (modifiable lvalue)
    char operator[]( int ) const; // subscript operator (rvalue)
    String operator()( int, int = 0 ) const; // return a substring
-   int length() const; // return string length
-   //private:
-   int slength; // string length (not counting null terminator)
+   int getLength() const; // return string length
+
+   // ============ Benny adding =============
+   String( const int x ); 
+   String( const int x, const int base );
+ 
+   const String &operator=( string right );
+   const String &operator=( const char *right );
+
+   String operator+( const int number );
+   String operator+( const char one );
+   String operator+( unsigned long number );
+   String operator+( String right );
+
+   char charAt(const int n);
+   int compareTo(const String s);
+   String concat(const String s); 	
+   bool endsWith(const String s);
+   bool equals(const String s);
+   bool equalsIgnoreCase(const String s);	
+   void getBytes(int buf[], int *len);	
+   int indexOf(char val); 
+   int indexOf(String val); 
+   int indexOf(char val, int from); 
+   int indexOf(String val, int from); 
+   int lastIndexOf(char val); 
+   int lastIndexOf(String val); 
+   int lastIndexOf(char val, int from); 
+   int lastIndexOf(String val, int from); 
+   int length(); 
+   String replace(String sub1, String sub2); 
+   void setCharAt(int index,char c); 
+   bool startsWith(String s); 
+   String substring(int from);
+   String substring(int from, int to);
+   void toCharArray(char buf[], int *len);
+   void toLowerCase(); 
+   void toUpperCase(); 
+   void trim(); 
+   char *getPointer(); 
+
+private:
+   int lngth; // string length (not counting null terminator)
    char *sPtr; // pointer to start of pointer-based string
 
    void setString( const char * ); // utility function
 
-   String replace(string, string );
-   void charAt(int pos); 
-   void compareTo(); 
-   void concat(); 
-   void endsWith(); 
-   void equals(); 
-   void equalsIgnoreCase(); 
-   void getBytes(); 
-   void indexOf(); 
-   void lastIndexOf(); 
-   void length(); 
-   String replace(char x,char y); 
-   String replace(const char *x,const char *y); 
-   void setCharAt(); 
-   void startsWith(); 
-   void substring(); 
-   void toCharArray(); 
-   void toLowerCase(); 
-   void toUpperCase(); 
-   void trim();
- 
+
+
 }; // end class String
+
